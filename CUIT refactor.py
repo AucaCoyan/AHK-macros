b7950b8e73171204edba68d1f298a1bf7708d1fc
AucaCoyan <30557287+AucaCoyan@users.noreply.github.com>
2020-06-24 11:41:00 -0300

    Add files via upload

---
+++ CUIT refactor.py
@@ -0,0 +1,15 @@
+import re
+import pyperclip
+
+# capture clipboard
+# check if it has a CUIT
+# copy to clipboard
+# go to nosis and paste the cuit, after, press enter
+
+cuitRegex = re.compile(r'\d{2}.\d{8}.\d{1}')
+text = str(pyperclip.paste())
+matches = []
+for groups in cuitRegex.findall(text):
+    matches.append(groups)
+
+print(matches)
